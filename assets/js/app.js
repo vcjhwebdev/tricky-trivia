35fd11df40cda4c46918ce660d2a7f828fc913a3
Esha Reddy <esha.reddy@warriorlife.net>
2019-01-29 08:56:45 -0800

    Changed color and added new page with javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
 var startGame = document.querySelector('.start-game');
+var overlay = document.querySelector('#overlay');
 
 startGame.addEventListener('click', function(e) {
   // e.target <-- thing you clicked
   // e.target.value <-- value attribute
   // e.target.textContent <-- text of thing
-  alert(e.target.value + " clicked");
+  // alert(e.target.value + " clicked");
+  overlay.style.display = "none";
 });
