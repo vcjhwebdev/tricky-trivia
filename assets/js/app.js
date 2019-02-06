1dabd1baebc3a2d459930b3b3801c424ff448a81
Esha Reddy <esha.reddy@warriorlife.net>
2019-02-06 09:01:38 -0800

    Added next button and uncommented some lines in javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,15 @@
 //var startGame = document.querySelector('.start-game');
-//var overlay = document.querySelector('#overlay');
+var overlay = document.querySelector('#overlay');
 //var categories = document.querySelector('#categories');
 //var aeasy = document.querySelector('.aeasy');
 //var animaleasy = document.querySelector('#animaleasy1');
 
-//startGame.addEventListener('click', function(e) {
+startGame.addEventListener('click', function(e) {
   // e.target <-- thing you clicked
   // e.target.value <-- value attribute
   // e.target.textContent <-- text of thing
   // alert(e.target.value + " clicked");
-  //overlay.style.display = "none";
+  overlay.style.display = "none";
   // catigories.style.display = "none";
   //levels1.style.display = "none";
   //animaleasy1.style.display = "none";
