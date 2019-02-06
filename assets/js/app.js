cf38a50b8b42d875c6e4ad79b9def92f0df83f36
Esha Reddy <esha.reddy@warriorlife.net>
2019-02-06 09:36:13 -0800

    tried to make animal and easy combination to move to another page -- failed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,30 @@
 //var startGame = document.querySelector('.start-game');
-var overlay = document.querySelector('#overlay');
+
 //var categories = document.querySelector('#categories');
-//var aeasy = document.querySelector('.aeasy');
 //var animaleasy = document.querySelector('#animaleasy1');
 
-startGame.addEventListener('click', function(e) {
+//startGame.addEventListener('click', function(e) {
   // e.target <-- thing you clicked
   // e.target.value <-- value attribute
   // e.target.textContent <-- text of thing
-  // alert(e.target.value + " clicked");
-  overlay.style.display = "none";
+  // alert(e.target.value + " clicked");  overlay.style.display = "none";
   // catigories.style.display = "none";
   //levels1.style.display = "none";
   //animaleasy1.style.display = "none";
+//});
+//});
+
+
+var next = document.querySelector('.next')
+var nextpage = document.querySelector('#nextpage')
+var aeasy = document.querySelector('.animal'+'.easy');
+
+next.addEventListener('click', function(e){
+  nextpage.style.display = "none";
+        //if (categories == animal)
+          //{alert("Do you like animals");}
+  aeasy.style.display = "none" {
+    {alert("HEy");}
+  };
+
 });
