b1d5865b90fd3a881369645258afc4c8f572c8ed
Esha Reddy <esha.reddy@warriorlife.net>
2019-02-12 09:22:04 -0800

    started to add new page through javascript for questions and deleted line breaks

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,10 +13,42 @@
   //animaleasy1.style.display = "none";
 //});
 //});
-if (easy1 == "cat")
-  {alert(confirm("Yes!!! You got the right answer!!! Click "));
-    if (confirm == )}
-else if (easy1 == "dog")
-  {alert("Try Again");}
-else if (easy1 == "lizard")
-  {alert("Try Again");}
+
+var settingsForm = document.getElementById('settings');
+
+settingsForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  var animal = document.getElementById('animal');
+  var food = document.getElementById('food');
+  var movies = document.getElementById('movies');
+  var easy = document.getElementById('easy');
+  var medium = document.getElementById('medium');
+  var hard = document.getElementById('hard');
+  var category = '';
+  var difficulty = '';
+
+  if(animal.checked) {
+    category = 'animal';
+  } else if(food.checked){
+    category = "food";
+  } else if(movies.checked){
+    category= = "movies";
+  }
+
+  if(easy.checked) {
+    difficulty = 'easy';
+  } else if(medium.checked){
+    difficulty = "medium";
+  } else if(hard.checked){
+    difficulty= = "hard";
+  }
+
+});
+//
+// if (easy1 == "cat")
+//   {alert(confirm("Yes!!! You got the right answer!!! Click "));
+//     if (confirm == )}
+// else if (easy1 == "dog")
+//   {alert("Try Again");}
+// else if (easy1 == "lizard")
+//   {alert("Try Again");}
