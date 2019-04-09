b39633dfeb50cea528965ea8adb4fc13d1cc5998
Esha Reddy <esha.reddy@warriorlife.net>
2019-04-09 09:52:26 -0700

    Added questions

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,13 +39,31 @@ var questions = {
 
       },
       {
-      text: "How do fish breathe underwater?",
-      options: [
-        'Mouth',
-        'Gills',
-        "They Don't"
-      ],
-      answer: 'Gills'
+        text: "How do fish breathe underwater?",
+        options: [
+          'Mouth',
+          'Gills',
+          "They Don't"
+        ],
+        answer: 'Gills'
+    },
+      {
+        text: "What is a male chicken called?",
+        options: [
+          'Chicken',
+          'Hen',
+          "Rooster"
+        ],
+        answer: 'Gills'
+    },
+      {
+        text: "Which of these dogs has spots?",
+        options: [
+          'Dalmatian',
+          'Golden Retriever',
+          "German Shepherd"
+        ],
+        answer: 'Gills'
     }
     ],
     medium: [
@@ -77,14 +95,14 @@ var questions = {
         answer: 'Love and Peace'
       },
       {
-      text: "What do otters use to open clams?",
-      options: [
-        'Rocks',
-        'Seashells',
-        'Seaweed'
-      ],
-      answer: 'Rocks'
-    },
+        text: "What do otters use to open clams?",
+        options: [
+          'Rocks',
+          'Seashells',
+          'Seaweed'
+        ],
+        answer: 'Rocks'
+      },
       {
         text: "Where do monarch butterflies fly in the winter?",
         options: [
@@ -93,7 +111,25 @@ var questions = {
           'East'
         ],
         answer: 'South'
-      }
+      },
+      {
+        text: "Which of these is a mammal that lays eggs?",
+        options: [
+          'Clownfish',
+          'Chicken',
+          "Platypus"
+        ],
+        answer: 'Gills'
+     },
+      {
+         text: "Which of these burds can reach the fastest speed?",
+         options: [
+           'Bald Eagle',
+           'Perigrine Falcon',
+           "Horned Sungem"
+         ],
+         answer: 'Perigrine Falcon'
+    }
     ],
     hard: [
       {
@@ -140,38 +176,57 @@ var questions = {
           'Necrosis'
         ],
         answer: 'Murder'
-      }
+      },
+      {
+        text: "Which animal has the highest blood pressure?",
+        options: [
+          'Blue Whale',
+          'Giraffe',
+          'Camel'
+        ],
+        answer: 'Murder'
+      },
+      {
+        text: "What is the smallest mammal in the world?",
+        options: [
+          'Western Harvest Mouse',
+          'Bumblebee Bat',
+          'Pygmy Marmoset'
+        ],
+        answer: 'Bumblebee Bat'
+      },
+
     ]
   },
   food:{
     easy:[
       {
-      text: 'Which of these is the most popular topping on pizza?',
-      options: [
-        'Apple Slices',
-        'Pepperoni',
-        'Noodles'
-      ],
-      answer: 'Pepperoni'
-    },
+        text: 'Which of these is the most popular topping on pizza?',
+        options: [
+          'Apple Slices',
+          'Pepperoni',
+          'Noodles'
+        ],
+        answer: 'Pepperoni'
+      },
       {
-    text: 'What food is used as the base for guacamole?',
-    options: [
-      'Avocado',
-      'Cherries',
-      'Broccoli'
-    ],
-    answer: 'Avocado'
-  },
+        text: 'What food is used as the base for guacamole?',
+        options: [
+          'Avocado',
+          'Cherries',
+          'Broccoli'
+        ],
+        answer: 'Avocado'
+      },
       {
-  text: 'What do herbivores eat?',
-  options: [
-    'Only Meat',
-    'Only Vegetables',
-    'Both Vegetables and Meat'
-  ],
-  answer: 'Only Vegetables'
-},
+        text: 'What do herbivores eat?',
+        options: [
+          'Only Meat',
+          'Only Vegetables',
+          'Both Vegetables and Meat'
+        ],
+        answer: 'Only Vegetables'
+      },
       {
         text: "What do most kids eat to celebrate their birthday?",
         options: [
@@ -189,46 +244,63 @@ var questions = {
           'Pork'
         ],
         answer: 'Beef'
+      },
+      {
+        text: "Which of these fruits has inedible seeds?",
+        options: [
+          'Apple',
+          'Strawberries',
+          'Pitaya'
+        ],
+        answer: 'Apple'
+      },
+      {
+        text: "Which of these is a vegetable?",
+        options: [
+          'Tomato',
+          'Rhubard',
+          'Asparagus'
+        ],
+        answer: 'Asparagus'
       }
-  ],
+    ],
     medium:[
       {
-      text: 'What is sushi traditionally wrapped with?',
-      options: [
-        'Seaweed',
-        'Cabbage',
-        'Spinach'
-      ],
-      answer: 'Seaweed'
-    },
+        text: 'What is sushi traditionally wrapped with?',
+        options: [
+          'Seaweed',
+          'Cabbage',
+          'Spinach'
+        ],
+        answer: 'Seaweed'
+      },
       {
-    text: 'What ingredient causes bread to rise?',
-    options: [
-      'Flour',
-      'Butter',
-      'Yeast'
-    ],
-    answer: 'Yeast'
-  },
+        text: 'What ingredient causes bread to rise?',
+        options: [
+          'Flour',
+          'Butter',
+          'Yeast'
+        ],
+        answer: 'Yeast'
+      },
       {
-      text: 'Foods rich in starch are know by what name?(Hint: Pasta and Bread)',
-      options: [
-        'Herbs',
-        'Carbohydrates',
-        'Calories'
-      ],
-      answer: 'Carbohydrates'
-    },
+        text: 'Foods rich in starch are know by what name?(Hint: Pasta and Bread)',
+        options: [
+          'Herbs',
+          'Carbohydrates',
+          'Calories'
+        ],
+        answer: 'Carbohydrates'
+      },
       {
         text: 'Which of these is the best source for calcium?',
-      options: [
-        'Yogurt',
-        'Chicken',
-        'Blueberries'
-      ],
-      answer: 'Yogurt'
-
-    },
+        options: [
+          'Yogurt',
+          'Chicken',
+          'Blueberries'
+        ],
+        answer: 'Yogurt'
+      },
       {
         text: "Which of these is NOT a real Ben & Jerry's ice cream flavor?",
         options: [
@@ -237,36 +309,55 @@ var questions = {
           'Salted Caramel'
         ],
         answer: 'Salted Caramel'
+      },
+      {
+        text: "Which type of pastry is boiled before it is baked?",
+        options: [
+          'Studel',
+          'Bagel',
+          'Croissant'
+        ],
+        answer: 'Bagel'
+      },
+      {
+        text: "Monterey Jack is a type of what?",
+        options: [
+          'Cheese',
+          'Vegetable',
+          'Starch'
+        ],
+        answer: 'Asparagus'
       }
+
 ],
     hard:[
       {
-      text: 'When was the blue M&M introduced?',
-      options: [
-        '1985',
-        '1995',
-        '1999'
-      ],
-      answer: '1995'
-    },
+        text: 'When was the blue M&M introduced?',
+        options: [
+          '1985',
+          '1995',
+          '1999'
+        ],
+        answer: '1995'
+      },
       {
-    text: 'Which fast food restaurant was literally founded in a broom closet?',
-    options: [
-      "Papa John's",
-      'Taco Bell',
-      "Wendy's"
-    ],
-    answer: "Papa John's"
-  },
+        text: 'Which fast food restaurant was literally founded in a broom closet?',
+        options: [
+          "Papa John's",
+          'Taco Bell',
+          "Wendy's"
+        ],
+        answer: "Papa John's"
+      },
       {
-      text: "'Beefsteak' is a variety of what?",
-      options: [
-        'Turkey',
-        'Cow Meat',
-        'Tomato'
-      ],
-      answer: 'Tomato'
-    },
+        text: "'Beefsteak' is a variety of what?",
+        options: [
+          'Turkey',
+          'Cow Meat',
+          'Tomato'
+        ],
+        answer: 'Tomato'
+      },
       {
         text: 'What is the the only edible food that is said to never spoil?',
         options: [
@@ -284,38 +375,56 @@ var questions = {
           'Milano'
         ],
         answer: 'Oreos'
+      },
+      {
+        text: "What is Gari?",
+        options: [
+          'Rice',
+          'Cassava',
+          'Asparagus'
+        ],
+        answer: 'Asparagus'
+      },
+      {
+        text: "What is the spiciest pepper in the worol according to the Guinness World Record?",
+        options: [
+          'Jalapeno',
+          'Cayenne',
+          'Carolina Reaper'
+        ],
+        answer: 'Carolina Reaper'
       }
     ]
   },
   movies:{
     easy:[
       {
-      text: 'Who is the main mermaid in <em>"The Little Mermaid"</em>?',
-      options: [
-        'Aria',
-        'Ariel',
-        'Merida'
-      ],
-      answer: 'Ariel'
-    },
+        text: 'Who is the main mermaid in <em>"The Little Mermaid"</em>?',
+        options: [
+          'Aria',
+          'Ariel',
+          'Merida'
+        ],
+        answer: 'Ariel'
+      },
       {
-      text: 'In <em>Cinderella</em>, what did the fairy godmother turn into a carriage?',
-      options: [
-        'Pumpkin',
-        'Watermelon',
-        'Cantaloupe'
-      ],
-      answer: 'Pumpkin'
-    },
+        text: 'In <em>Cinderella</em>, what did the fairy godmother turn into a carriage?',
+        options: [
+          'Pumpkin',
+          'Watermelon',
+          'Cantaloupe'
+        ],
+        answer: 'Pumpkin'
+      },
       {
-    text: "What did Maleficent turn in to in Disney's animated film, <em>Sleeping Beauty</em>?",
-    options: [
-      'Lizard',
-      'Dragon',
-      'Lion'
-    ],
-    answer: 'Dragon'
-  },
+        text: "What did Maleficent turn in to in Disney's animated film, <em>Sleeping Beauty</em>?",
+        options: [
+          'Lizard',
+          'Dragon',
+          'Lion'
+        ],
+        answer: 'Dragon'
+      },
       {
         text: 'Which movie features Cruella de Vil as the villain?',
         options: [
@@ -333,36 +442,45 @@ var questions = {
           'Bumper'
         ],
         answer: 'Thumper'
+      },
+      {
+        text: "Which of these is a vegetable?",
+        options: [
+          'Tomato',
+          'Rhubard',
+          'Asparagus'
+        ],
+        answer: 'Asparagus'
       }
   ],
     medium:[
       {
-      text: "Who is one of the good fairies in Disney's animated film, <em>Sleeping Beauty</em>?",
-      options: [
-        'Fiora',
-        'Fleur',
-        'Flora'
-      ],
-      answer: 'Flora'
-    },
+        text: "Who is one of the good fairies in Disney's animated film, <em>Sleeping Beauty</em>?",
+        options: [
+          'Fiora',
+          'Fleur',
+          'Flora'
+        ],
+        answer: 'Flora'
+      },
       {
-      text: "What is the prince's name in <em>Snow White and the Seven Dwarves</em>?",
-      options: [
-        'Prince Charming',
-        'Prince Florian',
-        'Prince Philip'
-      ],
-      answer: 'Prince Florian'
-    },
+        text: "What is the prince's name in <em>Snow White and the Seven Dwarves</em>?",
+        options: [
+          'Prince Charming',
+          'Prince Florian',
+          'Prince Philip'
+        ],
+        answer: 'Prince Florian'
+      },
       {
-    text: 'What movie are the characters Phoebus and Eseralda from?',
-    options: [
-      'The Hunchback of Notre Dame',
-      'The Black Cauldron',
-      'Alice in Wonderland'
-    ],
-    answer: 'The Hunchback of Notre Dame'
-  },
+        text: 'What movie are the characters Phoebus and Eseralda from?',
+        options: [
+          'The Hunchback of Notre Dame',
+          'The Black Cauldron',
+          'Alice in Wonderland'
+        ],
+        answer: 'The Hunchback of Notre Dame'
+      },
       {
         text: 'In Peter Pan, does Captain Hook have his hook on his left hand or right hand?',
         options: [
@@ -380,38 +498,56 @@ var questions = {
           'Dumbella'
         ],
         answer: 'Dumbella'
+      },
+      {
+        text: "What is the name of one of the eels who are Ursula’s sidekicks?",
+        options: [
+          'Flotsam',
+          'Bones',
+          'Ripper'
+        ],
+        answer: 'Flotsam'
+      },
+      {
+        text: "What is the name of Mulan’s horse?",
+        options: [
+          'Hank',
+          'Frank',
+          'Khan'
+        ],
+        answer: 'Flotsam'
       }
   ],
     hard:[
       {
-      text: 'What is the only Disney song to win a Grammy Award for Song of the Year?',
-      options: [
-        'A Whole New World',
-        'Under the Sea',
-        "I Can't Wait to be King"
-      ],
-      answer: 'A Whole New World'
-    },
+        text: 'What is the only Disney song to win a Grammy Award for Song of the Year?',
+        options: [
+          'A Whole New World',
+          'Under the Sea',
+          "I Can't Wait to be King"
+        ],
+        answer: 'A Whole New World'
+      },
       {
-      text: 'Which Disney movie was the first to have a soundtrack album?',
-      options: [
-        'The Little Mermaid',
-        'Pocahontas',
-        'Snow White and the Seven Dwarves'
-      ],
-      answer: 'Snow White and the Seven Dwarves'
-    },
+        text: 'Which Disney movie was the first to have a soundtrack album?',
+        options: [
+          'The Little Mermaid',
+          'Pocahontas',
+          'Snow White and the Seven Dwarves'
+        ],
+        answer: 'Snow White and the Seven Dwarves'
+      },
       {
-    text: "What is the name of the only female chef at Gusteau's restaurant in the film <em>Ratatouille</em>?",
-    options: [
-      'Camille',
-      'Colette',
-      'Carly'
-    ],
-    answer: 'Colette'
-  },
+        text: "What is the name of the only female chef at Gusteau's restaurant in the film <em>Ratatouille</em>?",
+        options: [
+          'Camille',
+          'Colette',
+          'Carly'
+        ],
+        answer: 'Colette'
+      },
       {
-        text: 'What country was Nemo taken to in <em>Finding Nemo</em>?',
+             text: 'What country was Nemo taken to in <em>Finding Nemo</em>?',
         options: [
           'America',
           'England',
@@ -427,7 +563,27 @@ var questions = {
           'Claire'
         ],
         answer: 'Dinah'
+      },
+      {
+        text: "Who is Pinocchio's buddy in Pinocchio?",
+        options: [
+          'Jiminy Cricket',
+          'Hank Beetle',
+          'Jerry Roach'
+        ],
+        answer: 'Dinah'
+      },
+      {
+        text: "Who is Ariel's dad?",
+        options: [
+          'Poseidon',
+          'King Triten',
+          'Nereus'
+        ],
+        answer: 'Dinah'
       }
+
+
     ]
 
   }
